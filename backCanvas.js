b46278d142d8e7176ca4894a069385fe944b4eb5
Ben Maltby <benmaltby@hotmail.co.uk>
2024-08-04 19:23:12 +0100

    Added footer

--- backCanvas.js
+++ backCanvas.js
@@ -2,20 +2,24 @@ let points = [];
 let r = 100;
 
 function setup(){
-	createCanvas(windowWidth + 10, windowHeight)
+	createCanvas(windowWidth+10, windowHeight)
 	colorMode(HSB)
+
+	// drawingContext.shadowBlur = 32;
 	
 	for (let i = 0; i < 20; i++){
+		let h = random(360)
 		points.push({
 			pos: createVector(random(r,width-r), random(r,height-r)),
 			vel: p5.Vector.fromAngle(random(TAU), 4),
-			col: color(random(360), 70, 100, 0.6)
+			col: color(h, 100, 100, 0.6),
+			shadowCol: color(h, 100, 100, 1)
 		})
 	}
 }
 
 function draw(){
-	background(0, 0, 10, 0.1)
+	background(0, 0, 10, 1)
 
 	for (let p of points){
 
@@ -23,6 +27,7 @@ function draw(){
 		if (p.pos.x-r<0||p.pos.x+r>width){p.vel.x *= -1}
 		if (p.pos.y-r<0||p.pos.y+r>height){p.vel.y *= -1}
 
+		// drawingContext.shadowColor = p.shadowCol
 		stroke(p.col)
 		strokeWeight(r*2)
 		point(p.pos.x, p.pos.y)
